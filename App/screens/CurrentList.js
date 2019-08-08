35f5b8a50702243ff5f4b65e362edc0a6ef1b2be
aidansoffe <aidansoffe@gmail.com>
2019-08-08 17:48:30 -0400

    keyboard AddItem bar

--- App/screens/CurrentList.js
+++ App/screens/CurrentList.js
@@ -1,27 +1,33 @@
 import React from 'react';
-import { View, Text, SafeAreaView, ScrollView } from 'react-native';
+import { SafeAreaView, KeyboardAvoidingView, FlatList,} from 'react-native';
 
 import nachos from '../data/nachos';
 import ListItem, {Separator} from '../component/ListItem';
+import AddItem from '../component/AddItem'
 
 export default() => {
 
   return (
-    <SafeAreaView>
-      <ScrollView>
-      {nachos.map((item, index) => (
-        <React.Fragment>
+    <SafeAreaView style={{ flex: 1}}>
+      <KeyboardAvoidingView
+      style={{ flex: 1}}
+      behavior= 'padding'
+      >
+     <AddItem />
+      <FlatList
+      data={nachos}
+      renderItem={({item, index}) => (
           <ListItem
-        key={item.id}
         name={item.name}
         onFavoritePress={() => alert('todo') }
         isFavorite={index<2}
         />
-          <Separator />
-        </React.Fragment>
-        
-      ))}
-      </ScrollView>
+        )}
+        keyExtractor={(item) => item.id}
+          ItemSeparatorComponent={() => <Separator />} 
+          />
+      </KeyboardAvoidingView>
+
     </SafeAreaView>
   )
 };
